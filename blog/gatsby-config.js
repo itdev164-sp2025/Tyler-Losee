510fc6a59503a895a0a4e4962ac27d4900608d13
LoseeT <loseet@gmatc.matc.edu>
2025-02-06 19:18:36 -0600

    Added Plugins

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -19,30 +19,35 @@ module.exports = {
       address: "PO Box 1234"
     }
   },
-  plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+    plugins: [
+      {
+        resolve: `gatsby-source-contentful`,
+        options: {
+          spaceId: `YOUR_SPACE_ID`,
+          accessToken: `YOUR_ACCESS_TOKEN`,
+        },
       },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      `gatsby-plugin-image`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
       },
-    },
-  ],
-}
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `gatsby-starter-default`,
+          short_name: `starter`,
+          start_url: `/`,
+          background_color: `#663399`,
+          display: `minimal-ui`,
+          icon: `src/images/gatsby-icon.png`,
+        },
+      },
+    ],
+  }
+  
